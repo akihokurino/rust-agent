615cf0b7fb5d8fd899adb3a2208ca70b293b4bab
akiho <aki030402@gmail.com>
2026-07-18 13:45:14 +0900

    create tool

--- src/agent.rs
+++ src/agent.rs
@@ -1,1 +1,2 @@
 pub mod llm;
+mod tool;
---
+++ src/agent/tool.rs
@@ -0,0 +1,10 @@
+use crate::types::errors::AgentError;
+use async_trait::async_trait;
+
+#[async_trait]
+pub trait Tool: Send + Sync {
+    fn name(&self) -> String;
+    fn description(&self) -> String;
+    fn input_schema(&self) -> serde_json::Value;
+    async fn execute(&self, input: serde_json::Value) -> Result<serde_json::Value, AgentError>;
+}
